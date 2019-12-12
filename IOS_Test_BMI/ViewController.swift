72b78a8ab794e2730129505b6bfe33a975d2ce48
Nisha Tandel <angeltandel64.nt@gmail.com>
2019-12-11 19:14:12 -0500

    INITIAL COMMIT

--- IOS_Test_BMI/ViewController.swift
+++ IOS_Test_BMI/ViewController.swift
@@ -1,7 +1,12 @@
 //
 //  ViewController.swift
 //  IOS_Test_BMI
-//
+
+// Student ID : 301094688
+// BMI TRACKING APPLICATION
+// Date : 11 DECEMBER 2019
+
+
 //  Created by Nisha Tandel on 2019-12-11.
 //  Copyright © 2019 Nisha TAndel. All rights reserved.
 //
